ddd65004fbcb154dd229fd8a7d852f61ca4b2d30
shyam0github <78136386+shyam0github@users.noreply.github.com>
2023-05-21 20:21:02 +0530

    update

--- backend/codeGenerator.js
+++ backend/codeGenerator.js
@@ -1,13 +1,13 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
 openai.createChatCompletion({
   model: "gpt-3.5-turbo",
-  messages: [{role: "user", content: "Write HTML, JS and bootstrap 5 code for a simple contacts page that takes in a name, email, and message and sends it to a database."}],
+  messages: [{role: "user", content: "Generate a paragraph on football worldcup."}],
 }).then((response) => {
     // console.log(response.data);
     console.log(response.data.choices[0].message);
